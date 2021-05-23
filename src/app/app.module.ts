2f63adce007c0830a12c7900c05b6c2904206140
LennartMac <lennartpopma@hotmail.com>
2021-05-23 16:06:59 +0200

    Added mdbootstrap shizzle and created diski-page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { DataTablesModule } from "angular-datatables";
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { DiskiPageComponent } from './diski-page/diski-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DiskiPageComponent
   ],
   imports: [
     BrowserModule,
     DataTablesModule,
+    MDBBootstrapModule.forRoot(),
     NgbModule
   ],
   providers: [],
---
+++ src/app/diski-page/diski-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-diski-page',
+  templateUrl: './diski-page.component.html',
+  styleUrls: ['./diski-page.component.css']
+})
+export class DiskiPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
